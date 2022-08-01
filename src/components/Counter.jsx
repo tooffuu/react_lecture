997a9405a3b2f1d29b6e1b132450fc13a537e470
unknown <thdud8866@naver.com>
2022-08-01 15:36:05 +0900

    useEffect 사용

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Counter({ counter, setCounter }) {
+  const [searchWord, setSearchWord] = useState("");
   const addCounter = () => {
     setCounter(counter + 1);
   };
+  const onChange = (e) => {
+    setSearchWord(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log("API 호출");
+  }, []);
+
+  useEffect(() => {
+    console.log("change input");
+  }, [searchWord]);
+
+  useEffect(() => {
+    console.log(counter);
+  }, [counter]);
+
   return (
     <div>
+      <input
+        type="text"
+        placeholder="검색어"
+        onChange={onChange}
+        value={searchWord}
+      />
       <div>{counter}번 클릭했습니다.</div>
       <button onClick={addCounter}>+</button>
     </div>
